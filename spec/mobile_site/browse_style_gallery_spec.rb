8642340c1ac549a061e1264790d2972ce76ce528
Mital <m.shah@modcloth.com>
2013-02-25 16:45:16 -0800

    Mital's commit

--- spec/mobile_site/browse_style_gallery_spec.rb
+++ spec/mobile_site/browse_style_gallery_spec.rb
@@ -1,12 +1,25 @@
 require 'spec/spec_helper'
 
+
 describe 'Browse style gallery' do
-  it 'Verify user see all 3 tabs' do
-    go_to_style_gallery_page
-
-    page.find('#featured').click
+ it 'Verify tabs go on clicked filter' do
+    page.should have_css("a#latest.active") # by default user is directed to latest filter on gallery
+    page.find('#featured').click                #user is directed to featured on clicking featured
+    page.should have_css("a#featured.active")
     sleep 10
-  end
+    page.find('#loved').click                    #user is directed to most loved on clicking most loved
+    page.should have_css("a#loved.active")
+    sleep 10
+    page.find('#latest').click                  #user is directed to latest on clicking latest
+    page.should have_css("a#latest.active")
+    sleep 10
+
+ end
+
+ it 'Verify one column layout' do
+   within("#gallery") do
+    page.find(".outfits.columns-1")
+    end
+
 end
-
-
+end
